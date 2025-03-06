1868a0a4f3692bf06e6ca85a75ed05ef1576c127
Harshithk09 <117326008+Harshithk09@users.noreply.github.com>
2025-03-06 15:08:13 -0800

    edited multiset.h

--- test_multiset.cc
+++ test_multiset.cc
@@ -1,71 +1,41 @@
 #include <gtest/gtest.h>
+#include <algorithm>
+#include <vector>
 
-#include "multiset.h"
+#include "multimap.h"
 
-TEST(Multiset, Empty) {
-    Multiset<int> mset;
+// Test one key
+TEST(Map, OneKey) {
+  Map<int, int> map;
+  std::vector<int> keys{2};
 
-    /* Should be fully empty */
-    EXPECT_EQ(mset.Empty(), true);
-    EXPECT_EQ(mset.Size(), 0);
-    EXPECT_THROW(mset.Count(42), std::exception);
+  for (auto i : keys) {
+    map.Insert(i, i);
+  }
+
+  EXPECT_EQ(map.Contains(2), true);
+  EXPECT_EQ(map.Get(2), keys[0]);
 }
 
-TEST(Multiset, OneKey) {
-  Multiset<int> mset;
+// Test multiple keys
+TEST(Map, MultipleKeys) {
+  Map<int, int> map;
+  std::vector<int> keys{2, 18, 42, 43};
 
-  /* Test some insertion */
-  mset.Insert(23);
-  mset.Insert(42);
-  mset.Insert(42);
-  EXPECT_EQ(mset.Empty(), false);
-  EXPECT_EQ(mset.Size(), 3);
-  EXPECT_EQ(mset.Min(), 23);
-  EXPECT_EQ(mset.Max(), 42);
-  EXPECT_EQ(mset.Count(42), 2);
+  // Insert a bunch of keys
+  for (auto i : keys) {
+    map.Insert(i, i);
+  }
+
+  // Check that every key is contained
+  std::random_shuffle(keys.begin(), keys.end());
+  for (auto i : keys) {
+    EXPECT_EQ(map.Contains(i), true);
+    EXPECT_EQ(map.Get(i), i);
+  }
 }
-
-TEST(MultisetTest, InvalidKey) {
-    Multiset ms;
-    ms.insert(5);
-    ms.insert(3);
-    ms.insert(7);
-
-    EXPECT_THROW(ms.floor(2), std::invalid_argument);
-    EXPECT_THROW(ms.ceil(8), std::invalid_argument);
-}
-
-TEST(MultisetTest, Floor) {
-    Multiset ms;
-    ms.insert(5);
-    ms.insert(3);
-    ms.insert(7);
-    ms.insert(3);  // Duplicate key
-
-    EXPECT_EQ(ms.floor(4), 3);
-    EXPECT_EQ(ms.floor(5), 5);
-    EXPECT_EQ(ms.floor(6), 5);
-    EXPECT_EQ(ms.floor(7), 7);
-    EXPECT_THROW(ms.floor(2), std::invalid_argument);  // No key <= 2
-}
-
-TEST(MultisetTest, Ceil) {
-    Multiset ms;
-    ms.insert(5);
-    ms.insert(3);
-    ms.insert(7);
-    ms.insert(3);  // Duplicate key
-
-    EXPECT_EQ(ms.ceil(4), 5);
-    EXPECT_EQ(ms.ceil(5), 5);
-    EXPECT_EQ(ms.ceil(6), 7);
-    EXPECT_EQ(ms.ceil(7), 7);
-    EXPECT_THROW(ms.ceil(8), std::invalid_argument);  // No key >= 8
-}
-
 
 int main(int argc, char *argv[]) {
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
-
